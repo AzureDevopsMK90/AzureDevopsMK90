3395bdbe9b1fdcde436f2b370e154c80e849b1e8
AzureDevopsMK <Kondamartin@gmail.com>
2024-03-15 18:23:17 -0400

    add git commands

---
+++ gitcmds.cmd
@@ -0,0 +1,10 @@
+git init: Initializes a new Git repository in the current directory.
+git clone <repository>: Clones a remote repository to your local machine.
+git add <file>: Adds a file to the staging area.
+git commit -m "<message>": Commits the changes in the staging area with a descriptive message.
+git status: Shows the status of your working directory and staging area.
+git diff: Shows the differences between your working directory and the last commit.
+git branch: Lists all branches in the repository.
+git checkout <branch>: Switches to a different branch.
+git merge <branch>: Merges changes from a different branch into the current branch.
+git pull: Fetches and merges changes from a remote repository.
